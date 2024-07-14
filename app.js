882b085ac1b0627cc082c70078365c10dfda78f6
noman719 <mnomanmirz1996@gmail.com>
2024-07-14 12:18:58 +0500

    use api

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-fetch("https://api.escuelajs.co/api/v1/products/")
+
+
+
+fetch('https://fakestoreapi.com/products')
     .then((data) => data.json())
     .then(result => {
         // console.log("result", result)
@@ -16,13 +19,13 @@ fetch("https://api.escuelajs.co/api/v1/products/")
 
                 parent.innerHTML +=`<div class="trend">
             <div class="head">
-                <h1>Trends <span>Mens wear</span></h1>
+                <h1>Trends <span>${products.category}</span></h1>
             </div>
             <div class="card">
                 <div class="crd">
-                    <img src=${products.images[0]}alt="">
+                    <img src=${products.image}alt="loading">
                     <div class="crdtext">
-                        <h2> Cutton trending Shirts</h2>
+                        <h2> ${products.title}</h2>
                         <i class="fa-solid fa-star"></i>
                         <i class="fa-solid fa-star"></i>
                         <i class="fa-solid fa-star"></i>
@@ -32,9 +35,9 @@ fetch("https://api.escuelajs.co/api/v1/products/")
                     </div>
                 </div>
                 <div class="crd">
-                    <img src="./img/mahdi-bafande-6f5KtX2GGv4-unsplash.jpg" alt="">
+                    <img src=${products.image} alt="">
                     <div class="crdtext">
-                        <h2> Cutton trending Shirts</h2>
+                        <h2>${products.title}</h2>
                         <i class="fa-solid fa-star"></i>
                         <i class="fa-solid fa-star"></i>
                         <i class="fa-solid fa-star"></i>
@@ -44,9 +47,9 @@ fetch("https://api.escuelajs.co/api/v1/products/")
                     </div>
                 </div>
                 <div class="crd">
-                    <img src="./img/logan-weaver-lgnwvr-G5RSe8i_Id8-unsplash.jpg" alt="">
+                    <img src=${products.image} alt="">
                     <div class="crdtext">
-                        <h2> Cutton trending Shirts</h2>
+                        <h2> ${products.title}</h2>
                         <i class="fa-solid fa-star"></i>
                         <i class="fa-solid fa-star"></i>
                         <i class="fa-solid fa-star"></i>
